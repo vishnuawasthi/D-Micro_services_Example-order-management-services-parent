81e0e2f94d166a64d8151d6d94137200edccdf72
vishnuawasthi <vishnuawasthi121@gmail.com>
2020-07-21 10:14:11 +0530

    Removed unwanted code

--- order-item-service/src/main/java/com/orderitem/repository/OrderItemRepository.java
+++ order-item-service/src/main/java/com/orderitem/repository/OrderItemRepository.java
@@ -10,15 +10,6 @@ import com.orderitem.entities.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
-	/**
-	 * @Query(value = "SELECT mbrship FROM MembershipDetails mbrship WHERE
-	 *              mbrship.memberDetails.id=:memberId") List<MembershipDetails>
-	 *              loadMembershipDetailByMemberId(@Param(value = "memberId") Long
-	 *              memberId);
-	 * 
-	 * @param orderId
-	 * @return
-	 */
 	@Query(" SELECT orderItem FROM OrderItem orderItem WHERE orderItem.orderId =:id")
 	List<OrderItem> findAllOrderItemsByOrderId(@Param(value = "id") Long orderId);
 }
